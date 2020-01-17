811d07e9886d2a527a13c14911590f9628d5168e
levans1 <45011820+levans1@users.noreply.github.com>
2020-01-17 13:52:49 -0800

    Add files via upload

---
+++ strangerthings_theme.rb
@@ -0,0 +1,20 @@
+# Welcome to Sonic Pi v3
+live_loop :strangerthings do
+  with_fx :distortion do
+  end
+  play :c2
+  sleep 0.25
+  play :e2
+  sleep 0.25
+  play :g2
+  sleep 0.25
+  play :b2
+  sleep 0.25
+  play :c3
+  sleep 0.25
+  play :g2
+  sleep 0.25
+  play :e2
+  sleep 0.25
+  play :c2
+end
